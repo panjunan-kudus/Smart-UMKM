ad57c045f12ae4b6f7b29bb4683b42450d646779
panjunan-kudus <kknpanjunanumk@gmail.com>
2026-07-08 19:51:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // TODO: Isi nilai placeholder ini setelah Google Sheets, Google Drive,
 // GitHub, dan Google Apps Script Web App selesai dibuat.
 window.APP_CONFIG = {
-  mode: "local",
+  mode: "gas",
   gasUrl: "https://script.google.com/macros/s/AKfycbxEG7c-j1El6RU8ymKDkosZfBlW4ocsFe93VE0oO4lWl308_rSVx4q0G8iDXJ-2-64Lvw/exec",
   githubRepo: "Smart-UMKM",
   sheetId: "1O4S1b1UpyIJB3bUwdb_zU-89yUVbKrCD8Q0X-4kWHIc",
