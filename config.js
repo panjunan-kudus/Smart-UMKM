06b137559e8a3bf386527427ed88c68364c34e0f
panjunan-kudus <kknpanjunanumk@gmail.com>
2026-07-08 19:49:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 window.APP_CONFIG = {
   mode: "local",
   gasUrl: "https://script.google.com/macros/s/AKfycbxEG7c-j1El6RU8ymKDkosZfBlW4ocsFe93VE0oO4lWl308_rSVx4q0G8iDXJ-2-64Lvw/exec",
-  githubRepo: "__REPOSITORY__",
+  githubRepo: "Smart-UMKM",
   sheetId: "1O4S1b1UpyIJB3bUwdb_zU-89yUVbKrCD8Q0X-4kWHIc",
   driveFolderId: "13ogBd3_k072KQxPBoA_az6eSwSB8ghwu"
 };
